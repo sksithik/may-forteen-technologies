52fb42a45f1d668a122f49e90784dea7ced5941f
sksithik <sksithik@gmail.com>
2020-06-15 05:35:32 +0100

    banner changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,22 @@ import { HomeComponent } from './pages/home/home.component';
 import { HomeResolver } from './pages/home/home-resolver.service';
 import { CmsRendererComponent } from './common/cmsRenderer/cms-renderer.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CmsRendererComponent,
-    BannerComponent
+    BannerComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
     ContentfulModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [HomeResolver],
   bootstrap: [AppComponent]
